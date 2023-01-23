c7ec3011baeb0c63728403e46c238f9f33a635d1
unknown <qinlinlin070@gmail.com>
2023-01-22 20:01:25 -0800

    666

---
+++ Lin_Qinlin_Program1A_HelloWorld.cpp/Lin_Qinlin_Program1A_HelloWorld.cpp
@@ -0,0 +1,15 @@
+// Qinlin Lin
+// CIS-5 Online
+// 2023-1-10
+// Program 1A: Hello World - This program displays a short message to the default output device
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World! " <<endl
+		<< "Programming is fun.";
+
+	return 0;
+}
